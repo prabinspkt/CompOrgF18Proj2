b14c454cca4b9554a208591670616fa7aefd14b5
Prabin Sapkota <professional_prabin@yahoo.com>
2018-11-07 22:27:39 -0500

    Added instructions to check if user input is more than 4 chars

--- project2.s
+++ project2.s
@@ -15,3 +15,7 @@ main:
     la $a0, input_storage                       # $a0 points to the starting address of user input
     li $v0, 8                                   # load code into $v0, $v0 is for user string input
     syscall
+
+    #Check if input is more than 4 characters long
+    lb $t0, 5($a0)                              # load the 6th byte into register $t0 , 5th byte is new line char when we use qtSpim to enter string
+    bne $zero, $t0, print_more_than_four        # if 6th byte is not NUL, user input has more than 4 char
