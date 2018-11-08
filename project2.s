5ef6b7805e535f2f03e5796c00da61ec0fb40401
Prabin Sapkota <professional_prabin@yahoo.com>
2018-11-07 22:24:24 -0500

    Added instructions to allocate space for user input

--- project2.s
+++ project2.s
@@ -6,3 +6,5 @@
     .asciiz "Input is empty."
     invalid_number:
     .asciiz "Invalid base-36 number."
+    input_storage:
+    .space 8                                    # reserves space for 8 bytes in memory for user_string
